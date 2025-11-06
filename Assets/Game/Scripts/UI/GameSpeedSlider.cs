246b31a63334c910097f6413da275fce68dcdbe7
THEOPHILE Nathan <ntheophilelb@gmail.com>
2025-11-06 23:23:55 +0100

    feat(Game): Time managed system with functionnal slider.

---
+++ Assets/Game/Scripts/UI/GameSpeedSlider.cs
@@ -0,0 +1,23 @@
+#region _____________________________/ INFOS
+//  AUTHOR : Nathan THEOPHILE (2025)
+//  Engine : Unity
+//  Singleton Script
+//  Note : MY_CONST, myPublic, m_MyProtected, _MyPrivate, lMyLocal, MyFunc(), pMyParam, onMyEvent, OnMyCallback, MyStruct
+#endregion
+
+using Rush.Game;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Rush.UI
+{
+    public class GameSpeedSlider : MonoBehaviour
+    {
+        private Slider _GameSpeedSlider;
+
+        void Start() { _GameSpeedSlider = GetComponentInParent<Slider>(); Debug.Log(_GameSpeedSlider); _GameSpeedSlider.onValueChanged.AddListener(OnSliderValueChanged); }
+
+        public void OnSliderValueChanged(float pValue) { Debug.Log(_GameSpeedSlider.value); TimeManager.instance.GlobalTickSpeed = pValue; }
+    }
+}
+
