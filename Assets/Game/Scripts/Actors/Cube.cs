c1d69e1dc9509caba4abe7213fefe6224a1920b3
THEOPHILE Nathan <ntheophilelb@gmail.com>
2025-11-09 16:53:59 +0100

    feat(Game): Added wall collisions.

--- Assets/Game/Scripts/Actors/Cube.cs
+++ Assets/Game/Scripts/Actors/Cube.cs
@@ -5,6 +5,8 @@
 //  Note : MY_CONST, myPublic, m_MyProtected, _MyPrivate, lMyLocal, MyFunc(), pMyParam, onMyEvent, OnMyCallback, MyStruct
 #endregion
 
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace Rush.Game
@@ -33,7 +35,16 @@ namespace Rush.Game
 
         #endregion
 
-    
+    // On utilise des directions logiques pr ne pas avoir à rotate le transform, on stocke les 4 directions dans une liste,
+    // on bouclera sur les 4 directions à partir de la direction actuelle en modulant par par 4 poru rester entre 0 et 3
+    // et pouvoir check facilement les 3 directions peut importe la direction actuel du cube
+    static readonly Vector3Int[] DIRECTIONS = { Vector3Int.forward, Vector3Int.right, Vector3Int.back, Vector3Int.left };
+    static int RightOf(int i) => (i + 1) % DIRECTIONS.Length;
+    static int BackOf(int i) => (i + 2) % DIRECTIONS.Length;
+    static int LeftOf(int i) => (i + 3) % DIRECTIONS.Length;
+    private int DirectionIndexOf(Vector3Int pDirection) => System.Array.IndexOf(DIRECTIONS, pDirection);
+
+
 
         void Start()
         {
@@ -46,6 +57,24 @@ namespace Rush.Game
         void Update()
         {
             Roll(currentTickStep);
+        }
+
+    private bool CheckForWall(Vector3Int pDirection) => Physics.Raycast(selfTransform.position, pDirection, _GridSize, _GroundLayer); //oeoe le raycast
+
+
+        /// <summary>
+        /// on se base sur la liste pour check les 4 directions depuis la direction actuelle et on sort si un checkwall renvoie false
+        /// </summary>
+        /// <param name="pCheckingOrder"></param>
+        private void FindNewDirection(IEnumerable<Vector3Int> pCheckingOrder)
+        {
+            foreach (var lDirection in pCheckingOrder)
+                if (!CheckForWall(lDirection)) //là il a trouvé une direction ou il prend rien dans la goule
+                {
+                    direction = lDirection;
+                    return;
+                }
+                else SetModePause(); //là il a mangé un truc dans la goule mdrrr
         }
 
         public void TickUpdate(int pTickIndex)
@@ -77,7 +106,8 @@ namespace Rush.Game
             transform.rotation = _EndRotation;
             transform.position = _Pivot + _EndPosition;
         }
-    
+    void SetModePause() { doAction = Pause; }
+        void Pause() { }
     }
 }
 
