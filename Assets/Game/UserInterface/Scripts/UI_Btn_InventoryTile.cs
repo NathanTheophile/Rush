e4bda15caa51264acee8ed104c6de622a107b802
Nathan Théophile <ntheophilelb@gmail.com>
2025-11-18 07:06:15 +0100

    feat(Inventory): Added tiles amount.

--- Assets/Game/UserInterface/Scripts/UI_Btn_InventoryTile.cs
+++ Assets/Game/UserInterface/Scripts/UI_Btn_InventoryTile.cs
@@ -5,6 +5,14 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public class UI_Btn_InventoryTile : MonoBehaviour
 {
-    [SerializeField] private TMP_Text _TileName;
-    [SerializeField] private TMP_Text _TileAmount;
+    [SerializeField] private TMP_Text _TxtTileName;
+    [SerializeField] private TMP_Text _TxtTileAmount;
+    [SerializeField] private int _TileAmount;
+
+    public void UpdateTileAmount()
+    {
+        _TileAmount --;
+        if (_TileAmount == 0) Destroy(gameObject);
+        _TxtTileAmount.text = _TileAmount.ToString();
+    }
 }
